617e3120dfecbea2af055816a97e173610e73c35
Tung Mai <tungm92@gmail.com>
2024-08-06 15:17:36 -0400

    fixed model and set up routes

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const testJWTRouter = require('./controllers/test-jwt');
-const usersRouter = require('./controllers/users');
-const profilesRouter = require('./controllers/profiles');
+const tracksRouter = require('./controllers/tracks');
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -17,9 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes go here
-app.use('/test-jwt', testJWTRouter);
-app.use('/users', usersRouter);
-app.use('/profiles', profilesRouter);
+app.use('/tracks', tracksRouter);
 
 app.listen(3000, () => {
     console.log('The express app is ready!');
